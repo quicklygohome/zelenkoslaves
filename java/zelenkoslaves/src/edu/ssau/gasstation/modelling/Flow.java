ef6584b5423257a88436092f81bc1b1cec68036d
quicklygohome <quicklygohome@users.noreply.github.com>
2016-12-13 14:11:26 +0400

    Добавлен поток

---
+++ java/zelenkoslaves/src/edu/ssau/gasstation/modelling/Flow.java
@@ -0,0 +1,11 @@
+package azs.modelling;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sasha on 30.11.2016.
+ */
+public interface Flow {
+    public List<Double> getValuesSequence(int n);
+}
