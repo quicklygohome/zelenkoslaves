5915ee56167663a8f8656e5f21d70a3562d5c262
quicklygohome <quicklygohome@users.noreply.github.com>
2016-12-17 16:12:01 +0400

    Дописан класс случайный поток

--- java/zelenkoslaves/src/edu/ssau/gasstation/modelling/RandomFlow.java
+++ java/zelenkoslaves/src/edu/ssau/gasstation/modelling/RandomFlow.java
@@ -7,10 +7,25 @@ import java.util.List;
  * Created by Sasha on 30.11.2016.
  */
 public class RandomFlow implements Flow {
+    private ProbabilityDistribution distribution;
+    public RandomFlow(ProbabilityDistribution distribution){
+        this.distribution = distribution;
+    }
+
+    public void setDistribution(ProbabilityDistribution distribution) {
+        this.distribution = distribution;
+    }
+
+    public ProbabilityDistribution getDistribution() {
+        return distribution;
+    }
+
     @Override
     public List<Double> getValuesSequence(int n) {
         List<Double> sequence = new ArrayList<>();
-
-        return null;
+        for (int i=0; i<n; i++){
+            sequence.add(distribution.generateValue());
+        }
+        return sequence;
     }
 }
