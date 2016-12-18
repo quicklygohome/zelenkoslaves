ca5709a229eac4afd6d1dfd9008d5f50326e541b
quicklygohome <quicklygohome@users.noreply.github.com>
2016-12-18 23:52:09 +0400

    Update

--- java/zelenkoslaves/src/edu/ssau/gasstation/GUI/model/ModelField.java
+++ java/zelenkoslaves/src/edu/ssau/gasstation/GUI/model/ModelField.java
@@ -1,15 +1,24 @@
 package edu.ssau.gasstation.GUI.model;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
+
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Sasha on 18.12.2016.
  */
 public class ModelField {
     public static final int CELL_SIZE = 32; //cell size in pixels
-    private static Cell[][] cells;
-    private Pane pane;
+    private Cell[][] cells;
+    private Canvas canvas;
+    private GraphicsContext gc;
     //sizes in cells!
     private int fieldSizeX;
     private int fieldSizeY;
@@ -17,14 +26,17 @@ public class ModelField {
     public ModelField(){
         this.fieldSizeX = 2;
         this.fieldSizeY = 2;
-        cells = new Cell[fieldSizeX][fieldSizeY];
+        this.cells = new Cell[fieldSizeX][fieldSizeY];
+        this.canvas = new Canvas(fieldSizeX*CELL_SIZE,fieldSizeY*CELL_SIZE);
+        this.gc = canvas.getGraphicsContext2D();
     }
 
-    public ModelField(int fieldSizeX, int fieldSizeY, Pane pane){
+    public ModelField(int fieldSizeX, int fieldSizeY){
         this.fieldSizeX = fieldSizeX;
         this.fieldSizeY = fieldSizeY;
-        this.pane = pane;
-        cells = new Cell[fieldSizeX][fieldSizeY];
+        this.cells = new Cell[fieldSizeX][fieldSizeY];
+        this.canvas = new Canvas(fieldSizeX*CELL_SIZE,fieldSizeY*CELL_SIZE);
+        this.gc = canvas.getGraphicsContext2D();
     }
 
     public void setFieldSizeX(int fieldSizeX) {
@@ -47,12 +59,27 @@ public class ModelField {
         return cells[x][y].getState();
     }
 
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public GraphicsContext getGc() {
+        return gc;
+    }
+
+    public void setGc(GraphicsContext gc) {
+        this.gc = gc;
+    }
+
+    public void setCanvas(Canvas canvas) {
+        this.canvas = canvas;
+    }
+
     public void setState(int x, int y, int state){
         cells[x][y].setState(state);
     }
 
     public void init(){
-        pane.setPrefSize(fieldSizeX*CELL_SIZE, fieldSizeY*CELL_SIZE);
         for (int i = 0; i < fieldSizeX; i++){
             for (int j=0; j<fieldSizeY; j++){
                 cells[i][j] = new Cell(i*CELL_SIZE, j*CELL_SIZE, 0);
@@ -63,14 +90,31 @@ public class ModelField {
     public void draw(){
         for (Cell[] line : cells){
             for (Cell cell : line){
-                drawElment(cell);
+                drawElment(cell, this.gc);
             }
         }
     }
 
-    private static void drawElment(Cell elem){
+    private static void drawElment(Cell elem, GraphicsContext gc){
         if (elem.getSprite() == null) return;
-        //отрисовка
+        else {
+            gc.drawImage(toImage(elem), elem.getX(), elem.getY()); //отрисовка
+        }
+    }
 
+    private static Image toImage(Cell cell){
+        BufferedImage bufferedImage = cell.getSprite().getSprite();
+        WritableImage wr = null;
+        if (bufferedImage != null) {
+            wr = new WritableImage(bufferedImage.getWidth(), bufferedImage.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < bufferedImage.getWidth(); x++) {
+                for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                    pw.setArgb(x, y, bufferedImage.getRGB(x, y));
+                }
+            }
+        }
+        ImageView imageView = new ImageView(wr);
+        return imageView.getImage();
     }
 }
